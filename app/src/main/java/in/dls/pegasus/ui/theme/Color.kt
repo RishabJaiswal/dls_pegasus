8694f6a1958344374353e017eed9ffa6487441ac
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-01-11 23:16:08 +0530

    Add base color tokens

--- app/src/main/java/in/dls/pegasus/ui/theme/Color.kt
+++ app/src/main/java/in/dls/pegasus/ui/theme/Color.kt
@@ -6,3 +6,39 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+//greyscale
+val Grey100 = Color(0xFFFCFCFC)
+val Grey200 = Color(0xFFF7F7FC)
+val Grey300 = Color(0xFFEFF0F6)
+val Grey400 = Color(0xFFD9DBE9)
+val Grey500 = Color(0xFFA0A3BD)
+val Grey600 = Color(0xFF6E7191)
+val Grey700 = Color(0xFF4E4B66)
+val Grey800 = Color(0xFF14142B)
+
+//violet
+val Violet100 = Color(0xFFBCA4FF)
+val Violet500 = Color(0xFF5F2EEA)
+val Violet800 = Color(0xFF2A00A2)
+
+//blue
+val Blue100 = Color(0xFF82E9FF)
+val Blue500 = Color(0xFF1CC8EE)
+val Blue800 = Color(0xFF0096B7)
+
+//pink
+val Pink100 = Color(0xFFFF84B7)
+val Pink500 = Color(0xFFED2E7E)
+val Pink800 = Color(0xFFC30052)
+
+//green
+val Green100 = Color(0xFF34EAB9)
+val Green500 = Color(0xFF00BA88)
+val Green800 = Color(0xFF00966D)
+
+//yellow
+val Yellow100 = Color(0xFFFFD789)
+val Yellow500 = Color(0xFFF4B740)
+val Yellow800 = Color(0xFF946200)
+
