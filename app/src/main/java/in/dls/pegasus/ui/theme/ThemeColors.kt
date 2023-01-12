f971a6d470213a897d1190f04b6a889617e4aae9
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-01-12 17:55:45 +0530

    Convert 'DlsColors' to a data class

--- app/src/main/java/in/dls/pegasus/ui/theme/ThemeColors.kt
+++ app/src/main/java/in/dls/pegasus/ui/theme/ThemeColors.kt
@@ -1,75 +1,87 @@
 package `in`.dls.pegasus.ui.theme
 
-import androidx.compose.ui.graphics.Color
-
-fun lightDlsColors(
-    titleActive: Color = Grey800,
-    body: Color = Grey700,
-    label: Color = Grey600,
-    placeholder: Color = Grey500,
-    line: Color = Grey400,
-    inputBackground: Color = Grey300,
-    background: Color = Grey200,
-    offWhite: Color = Grey100,
-
-    //primary
-    primaryDefault: Color = Purple500,
-    primaryDark: Color = Purple800,
-    primaryLight: Color = Purple50,
-    primaryBg: Color = Purple25,
-
-    //secondary
-    secondaryDefault: Color = Blue500,
-    secondaryDark: Color = Blue800,
-    secondaryLight: Color = Blue50,
-    secondaryBg: Color = Blue25,
-
-    //error
-    errorDefault: Color = Pink500,
-    errorDark: Color = Pink800,
-    errorLight: Color = Pink50,
-    errorBg: Color = Pink25,
-
-    //success
-    successDefault: Color = Green500,
-    successDark: Color = Green800,
-    successBg: Color = Green50,
-    successLight: Color = Green25,
-
-    //warning
-    warningDefault: Color = Yellow500,
-    warningDark: Color = Yellow800,
-    warningBg: Color = Yellow50,
-    warningLight: Color = Yellow25,
-) =
+fun lightDlsColors() =
     DlsColors(
-        titleActive = titleActive,
-        body = body,
-        label = label,
-        placeholder = placeholder,
-        line = line,
-        inputBackground = inputBackground,
-        background = background,
-        offWhite = offWhite,
-        primaryDefault = primaryDefault,
-        primaryDark = primaryDark,
-        primaryLight = primaryLight,
-        primaryBg = primaryBg,
-        secondaryDefault = secondaryDefault,
-        secondaryDark = secondaryDark,
-        secondaryBg = secondaryBg,
-        secondaryLight = secondaryLight,
-        errorDefault = errorDefault,
-        errorDark = errorDark,
-        errorBg = errorBg,
-        errorLight = errorLight,
-        successDefault = successDefault,
-        successDark = successDark,
-        successBg = successBg,
-        successLight = successLight,
-        warningDefault = warningDefault,
-        warningDark = warningDark,
-        warningBg = warningBg,
-        warningLight = warningLight,
         isLight = true,
+        titleActive = Grey800,
+        body = Grey700,
+        label = Grey600,
+        placeholder = Grey500,
+        line = Grey400,
+        inputBackground = Grey300,
+        background = Grey200,
+        offWhite = Grey100,
+
+        // primary
+        primaryDefault = Purple500,
+        primaryDark = Purple800,
+        primaryLight = Purple50,
+        primaryBg = Purple25,
+
+        // secondary
+        secondaryDefault = Blue500,
+        secondaryDark = Blue800,
+        secondaryLight = Blue50,
+        secondaryBg = Blue25,
+
+        // error
+        errorDefault = Pink500,
+        errorDark = Pink800,
+        errorLight = Pink50,
+        errorBg = Pink25,
+
+        // success
+        successDefault = Green500,
+        successDark = Green800,
+        successBg = Green50,
+        successLight = Green25,
+
+        // warning
+        warningDefault = Yellow500,
+        warningDark = Yellow800,
+        warningBg = Yellow50,
+        warningLight = Yellow25,
+    )
+
+fun darkDlsColors() =
+    DlsColors(
+        isLight = false,
+        titleActive = Grey800,
+        body = Grey700,
+        label = Grey600,
+        placeholder = Grey500,
+        line = Grey400,
+        inputBackground = Grey300,
+        background = Grey200,
+        offWhite = Grey100,
+
+        // primary
+        primaryDefault = Purple500,
+        primaryDark = Purple800,
+        primaryLight = Purple50,
+        primaryBg = Purple25,
+
+        // secondary
+        secondaryDefault = Blue500,
+        secondaryDark = Blue800,
+        secondaryLight = Blue50,
+        secondaryBg = Blue25,
+
+        // error
+        errorDefault = Pink500,
+        errorDark = Pink800,
+        errorLight = Pink50,
+        errorBg = Pink25,
+
+        // success
+        successDefault = Green500,
+        successDark = Green800,
+        successBg = Green50,
+        successLight = Green25,
+
+        // warning
+        warningDefault = Yellow500,
+        warningDark = Yellow800,
+        warningBg = Yellow50,
+        warningLight = Yellow25,
     )
