1913bf3911aa79786d1446fe19318273b9f976df
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-01-12 17:59:17 +0530

    Refacto `PegasusTheme` -> `DlsTheme`

--- app/src/main/java/in/dls/pegasus/ui/theme/Theme.kt
+++ app/src/main/java/in/dls/pegasus/ui/theme/Theme.kt
@@ -1,27 +1,22 @@
 package `in`.dls.pegasus.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors()
-
-private val LightColorPalette = lightColors()
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 
 @Composable
-fun PegasusTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun DlsTheme(
+    colors: DlsColors = DlsTheme.colors,
+    content: @Composable () -> Unit,
+) {
+  CompositionLocalProvider(
+      LocalDlsColors provides colors,
+  ) {
+    content()
+  }
+}
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+object DlsTheme {
+  val colors: DlsColors
+    @Composable @ReadOnlyComposable get() = LocalDlsColors.current
 }
