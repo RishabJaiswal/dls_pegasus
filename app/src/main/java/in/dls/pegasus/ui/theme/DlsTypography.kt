e488e66c3f502ba86c078163dfd79cabb3a8c3c4
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-01-12 20:49:54 +0530

    Create data class for DLS Typography

---
+++ app/src/main/java/in/dls/pegasus/ui/theme/DlsTypography.kt
@@ -0,0 +1,20 @@
+package `in`.dls.pegasus.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+
+data class DlsTypography(
+    val displayLargeBold: TextStyle,
+    val displayMediumBold: TextStyle,
+    val displaySmallBold: TextStyle,
+    val displayLarge: TextStyle,
+    val displayMedium: TextStyle,
+    val displaySmall: TextStyle,
+    val textLarge: TextStyle,
+    val textMedium: TextStyle,
+    val textSmall: TextStyle,
+    val textXSmall: TextStyle,
+    val linkLarge: TextStyle,
+    val linkMedium: TextStyle,
+    val linkSmall: TextStyle,
+    val linkXSmall: TextStyle,
+)
