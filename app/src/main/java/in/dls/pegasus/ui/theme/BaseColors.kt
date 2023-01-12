18e763d629875ce409fe671ede5db3378203a65a
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-01-12 14:35:29 +0530

    Add more purple, blue, pink, green & yellow base colors

--- app/src/main/java/in/dls/pegasus/ui/theme/BaseColors.kt
+++ app/src/main/java/in/dls/pegasus/ui/theme/BaseColors.kt
@@ -13,26 +13,36 @@ val Grey700 = Color(0xFF4E4B66)
 val Grey800 = Color(0xFF14142B)
 
 //violet
-val Violet100 = Color(0xFFBCA4FF)
-val Violet500 = Color(0xFF5F2EEA)
-val Violet800 = Color(0xFF2A00A2)
+val Purple25 = Color(0xFFF3EFFF)
+val Purple50 = Color(0xFFE4DAFF)
+val Purple100 = Color(0xFFBCA4FF)
+val Purple500 = Color(0xFF5F2EEA)
+val Purple800 = Color(0xFF2A00A2)
 
 //blue
+val Blue25 = Color(0xFFF1FCFF)
+val Blue50 = Color(0xFFDEF9FF)
 val Blue100 = Color(0xFF82E9FF)
 val Blue500 = Color(0xFF1CC8EE)
 val Blue800 = Color(0xFF0096B7)
 
 //pink
+val Pink25 = Color(0xFFFFF3F8)
+val Pink50 = Color(0xFFFFDFED)
 val Pink100 = Color(0xFFFF84B7)
 val Pink500 = Color(0xFFED2E7E)
 val Pink800 = Color(0xFFC30052)
 
 //green
+val Green25 = Color(0xFFF2FFFB)
+val Green50 = Color(0xFFDFFFF6)
 val Green100 = Color(0xFF34EAB9)
 val Green500 = Color(0xFF00BA88)
 val Green800 = Color(0xFF00966D)
 
 //yellow
+val Yellow25 = Color(0xFFFFF9EF)
+val Yellow50 = Color(0xFFFFF4DF)
 val Yellow100 = Color(0xFFFFD789)
 val Yellow500 = Color(0xFFF4B740)
 val Yellow800 = Color(0xFF946200)
