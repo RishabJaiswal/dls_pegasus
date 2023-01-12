4721af70cd58a1695439bccdc6c63864cf38efc8
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-01-12 22:50:17 +0530

    Create `corners` in `DlsTheme` (#3)
    * Create data class `DlsCorners` for corners in `DlsTheme`
    
    * Add corners in `DlsTheme`

---
+++ app/src/main/java/in/dls/pegasus/ui/theme/DlsCorners.kt
@@ -0,0 +1,17 @@
+package `in`.dls.pegasus.ui.theme
+
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.ui.unit.dp
+
+data class DlsCorners(
+    val small: CornerSize,
+    val medium: CornerSize,
+    val large: CornerSize,
+)
+
+fun mobileDlsCorners() =
+    DlsCorners(
+        small = CornerSize(8.dp),
+        medium = CornerSize(16.dp),
+        large = CornerSize(32.dp),
+    )
