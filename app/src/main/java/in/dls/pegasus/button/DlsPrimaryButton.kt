250a570db10d3a38f7610f69c090d2d48b5a8142
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-01-13 14:48:30 +0530

    Create `DlsButton` for primary type

--- app/src/main/java/in/dls/pegasus/button/DlsPrimaryButton.kt
+++ app/src/main/java/in/dls/pegasus/button/DlsPrimaryButton.kt
@@ -1,11 +1,11 @@
 package `in`.dls.pegasus.button
 
-import `in`.dls.pegasus.theme.DlsTheme
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import `in`.dls.pegasus.theme.DlsTheme
 
 @Preview
 @Composable
@@ -16,18 +16,18 @@ fun DlsPrimaryButton(
 ) {
   Button(
       onClick = onClick,
-      contentPadding = DlsButtonStyle.getButtonPadding(size),
+      contentPadding = size.getButtonPadding(),
       shape = DlsButtonStyle.shape,
       colors =
-      ButtonDefaults.buttonColors(
-          backgroundColor = DlsTheme.colors.primaryDefault,
-      ),
+          ButtonDefaults.buttonColors(
+              backgroundColor = DlsTheme.colors.primaryDefault,
+          ),
       content = {
-          Text(
-              text = text,
-              color = DlsTheme.colors.background,
-              style = DlsButtonStyle.textStyle(size),
-          )
+        Text(
+            text = text,
+            color = DlsTheme.colors.background,
+            style = DlsTheme.typography.textMedium,
+        )
       },
   )
 }
