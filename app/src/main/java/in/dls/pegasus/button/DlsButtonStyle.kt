f57b55c4bff137edb77f8c30c6f98a2be44af16a
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-01-13 15:31:37 +0530

    Add method to get button padding in `DlsButtonStyle`

--- app/src/main/java/in/dls/pegasus/button/DlsButtonStyle.kt
+++ app/src/main/java/in/dls/pegasus/button/DlsButtonStyle.kt
@@ -1,12 +1,22 @@
 package `in`.dls.pegasus.button
 
 import `in`.dls.pegasus.theme.DlsTheme
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 
 internal object DlsButtonStyle {
   val shape = RoundedCornerShape(50)
+
+  fun getButtonPadding(size: DlsButtonSize): PaddingValues {
+    return when (size) {
+      DlsButtonSize.LARGE -> PaddingValues(32.dp, 18.dp)
+      DlsButtonSize.MEDIUM -> PaddingValues(32.dp, 14.dp)
+      DlsButtonSize.SMALL -> PaddingValues(24.dp, 8.dp)
+    }
+  }
 
   @Composable
   fun textStyle(size: DlsButtonSize): TextStyle {
