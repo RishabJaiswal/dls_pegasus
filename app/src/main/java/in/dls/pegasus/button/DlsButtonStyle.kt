bf1cd506da5800cf2777013d0c4f2fb6ec18088f
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-01-13 15:08:51 +0530

    Create & use textstyle in `DlsButtonStyle`

--- app/src/main/java/in/dls/pegasus/button/DlsButtonStyle.kt
+++ app/src/main/java/in/dls/pegasus/button/DlsButtonStyle.kt
@@ -1,7 +1,10 @@
 package `in`.dls.pegasus.button
 
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import `in`.dls.pegasus.theme.DlsTheme
 
 internal object DlsButtonStyle {
-     val shape = RoundedCornerShape(50)
+  val shape = RoundedCornerShape(50)
+  @Composable fun textStyle() = DlsTheme.typography.textMedium
 }
