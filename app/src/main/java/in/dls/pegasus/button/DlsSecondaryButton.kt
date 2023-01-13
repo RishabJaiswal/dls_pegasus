3474cfaac616a4fa0d1c398412e409cb4f0f7a2f
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-01-13 15:10:31 +0530

    Create `DlsButton` for secondary type

---
+++ app/src/main/java/in/dls/pegasus/button/DlsSecondaryButton.kt
@@ -0,0 +1,41 @@
+package `in`.dls.pegasus.button
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import `in`.dls.pegasus.theme.DlsTheme
+
+@Preview
+@Composable
+fun DlsSecondaryButton(
+    text: String = "Button",
+    size: DlsButtonSize = DlsButtonSize.MEDIUM,
+    onClick: () -> Unit = {}
+) {
+  OutlinedButton(
+      onClick = onClick,
+      contentPadding = size.getButtonPadding(),
+      shape = DlsButtonStyle.shape,
+      colors =
+          ButtonDefaults.outlinedButtonColors(
+              backgroundColor = Color.Transparent,
+          ),
+      border =
+          BorderStroke(
+              width = 2.dp,
+              color = DlsTheme.colors.primaryDefault,
+          ),
+      content = {
+        Text(
+            text = text,
+            color = DlsTheme.colors.primaryDefault,
+            style = DlsButtonStyle.textStyle(),
+        )
+      },
+  )
+}
