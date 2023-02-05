17613987f09ee5b81884b9741a0c0dec7bece670
Rishab Jaiswal <shivam.jaiswal175@gmail.com>
2023-02-05 18:24:15 +0530

    Change bg color of `DlsPrimaryButton` based on state

--- app/src/main/java/in/dls/pegasus/MainActivity.kt
+++ app/src/main/java/in/dls/pegasus/MainActivity.kt
@@ -3,41 +3,33 @@ package `in`.dls.pegasus
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import `in`.dls.pegasus.button.DlsPrimaryButton
 import `in`.dls.pegasus.theme.DlsTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            DlsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      DlsTheme {
+        // A surface container using the 'background' color from the theme
+        DlsPrimaryButton(
+            text = "Hello"
+        )
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+  Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    DlsTheme {
-        Greeting("Android")
-    }
+  DlsTheme { Greeting("Android") }
 }
--- app/src/main/java/in/dls/pegasus/button/DlsPrimaryButton.kt
+++ app/src/main/java/in/dls/pegasus/button/DlsPrimaryButton.kt
@@ -1,33 +1,46 @@
 package `in`.dls.pegasus.button
 
-import `in`.dls.pegasus.theme.DlsTheme
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import `in`.dls.pegasus.theme.DlsTheme
 
 @Preview
 @Composable
 fun DlsPrimaryButton(
-    text: String = "",
-    size: DlsButtonSize = DlsButtonSize.MEDIUM,
-    onClick: () -> Unit = {}
+  text: String = "",
+  size: DlsButtonSize = DlsButtonSize.MEDIUM,
+  onClick: () -> Unit = {}
 ) {
+  val interactionSource = remember { MutableInteractionSource() }
+  val isPressed by interactionSource.collectIsPressedAsState()
+
+  val bgColor = when {
+    isPressed -> DlsTheme.colors.titleActive
+    else -> DlsTheme.colors.primaryDefault
+  }
+
   Button(
-      onClick = onClick,
-      contentPadding = DlsButtonStyle.getButtonPadding(size),
-      shape = DlsButtonStyle.shape,
-      colors =
+    onClick = onClick,
+    interactionSource = interactionSource,
+    contentPadding = DlsButtonStyle.getButtonPadding(size),
+    shape = DlsButtonStyle.shape,
+    colors =
       ButtonDefaults.buttonColors(
-          backgroundColor = DlsTheme.colors.primaryDefault,
+        backgroundColor = bgColor,
       ),
-      content = {
-          Text(
-              text = text,
-              color = DlsTheme.colors.background,
-              style = DlsButtonStyle.textStyle(size),
-          )
-      },
+    content = {
+      Text(
+        text = text,
+        color = DlsTheme.colors.background,
+        style = DlsButtonStyle.textStyle(size),
+      )
+    },
   )
 }
